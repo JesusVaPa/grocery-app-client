cacf5d7cfc89e345727106b5fceb78c6cc2ea3a0
Jesus Vazquez <vapa.jesusangel@gmail.com>
2024-10-15 18:07:50 -0400

    Created httpReq.js

---
+++ src/utils/httpReq.js
@@ -0,0 +1,21 @@
+function httpReq(method_name, path_str, body_map){
+	let req_map = {
+		method : method_name,
+		url : 'http://127.0.0.1:3000' + (path_str ? path_str : '')
+	};
+
+	if(body_map){
+		req_map.data = body_map;
+	}
+	// eslint-disable-next-line no-undef
+	return axios(req_map)
+	.then(function(response) {
+		return response.data;
+	})
+	.catch(function(error) {
+		console.error(error);
+	})
+	;
+}
+
+export default httpReq;
