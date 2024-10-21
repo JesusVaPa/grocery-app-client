275c06254e585f077645bbd0b11f4844036ce390
Jesus Vazquez <vapa.jesusangel@gmail.com>
2024-10-21 14:59:35 -0400

    Add css import

--- src/components/GroceryList.jsx
+++ src/components/GroceryList.jsx
@@ -1,14 +1,10 @@
 /* eslint-disable react/prop-types */
 import GroceryItem from './GroceryItem';
-import SortWidget from './SortWidget'; 
 import '../CSS/GroceryList.css';
 
-function GroceryList({ itemList, dispatch, onSortChange }) {
+function GroceryList({ itemList, dispatch }) {
   return (
     <>
-      {itemList.length > 0 && (
-        <SortWidget onSortChange={onSortChange} />
-      )}
       <ul>
         {
           itemList.map(function(item_map) {
