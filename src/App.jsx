b73727a44cd4563172b4ad7518e42d33d03ca854
Jesus Vazquez <vapa.jesusangel@gmail.com>
2024-10-21 14:59:09 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -14,25 +14,7 @@ function App() {
         dispatch({ type: 'init', body: item_list });
       });
   }, []);
-
-  const handleSortChange = (sortType) => {
-    let sortedList;
-    switch (sortType) {
-      case 'alphabetical':
-        sortedList = [...itemList].sort((a, b) => a.name.localeCompare(b.name));
-        break;
-      case 'date-asc':
-        sortedList = [...itemList].sort((a, b) => new Date(a.date) - new Date(b.date));
-        break;
-      case 'date-desc':
-        sortedList = [...itemList].sort((a, b) => new Date(b.date) - new Date(a.date));
-        break;
-      default:
-        sortedList = itemList;
-    }
-    dispatch({ type: 'init', body: sortedList });
-  };
-
+  
   return (
     <>
       <h1>My Grocery App</h1>
@@ -41,7 +23,6 @@ function App() {
         <GroceryList
           itemList={itemList}
           dispatch={dispatch}
-          onSortChange={handleSortChange} 
         />
       )}
     </>
