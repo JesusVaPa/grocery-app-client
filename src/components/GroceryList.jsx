0dbb18c530eaa5c63fe1cb3fca288810d31d8679
Jesus Vazquez <vapa.jesusangel@gmail.com>
2024-10-21 12:18:28 -0400

    Implemented onSortChange

--- src/components/GroceryList.jsx
+++ src/components/GroceryList.jsx
@@ -1,22 +1,29 @@
 /* eslint-disable react/prop-types */
 import GroceryItem from './GroceryItem';
+import SortWidget from './SortWidget'; // Asegúrate de importar el nuevo componente
 import '../CSS/GroceryList.css';
 
-function GroceryList({itemList, dispatch}){
+// eslint-disable-next-line react/prop-types
+function GroceryList({ itemList, dispatch, onSortChange }) {
   return (
+    <>
+      {itemList.length > 0 && (
+        <SortWidget onSortChange={onSortChange} />
+      )}
       <ul>
         {
-          itemList.map(function(item_map){
-          return(
-            <GroceryItem
-            key = { item_map.id} 
-            itemMap = {item_map}
-            dispatch = {dispatch}
-            />
-          );
-        })
-      }
-    </ul>
+          itemList.map(function(item_map) {
+            return (
+              <GroceryItem
+                key={item_map.id}
+                itemMap={item_map}
+                dispatch={dispatch}
+              />
+            );
+          })
+        }
+      </ul>
+    </>
   );
 }
 
