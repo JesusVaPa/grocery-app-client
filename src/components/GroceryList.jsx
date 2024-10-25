c0b612cac9173b5957d9a87956da3d0d4f6423aa
Jesus Vazquez <vapa.jesusangel@gmail.com>
2024-10-24 23:12:08 -0400

    Implemented logic to view change

--- src/components/GroceryList.jsx
+++ src/components/GroceryList.jsx
@@ -1,12 +1,12 @@
-/* eslint-disable react/prop-types */
 import GroceryItem from './GroceryItem';
-import '../css/GroceryList.css';
+import '../CSS/GroceryList.css';
 
-function GroceryList({ itemList, dispatch, viewMode }) { 
+// eslint-disable-next-line react/prop-types
+function GroceryList({ itemList, dispatch, viewMode }) {
 
   const groupByDate = (items) => {
     return items.reduce((grouped, item) => {
-      const dateKey = new Date(item.date).toLocaleDateString(); 
+      const dateKey = new Date(item.date).toLocaleDateString();
       if (!grouped[dateKey]) {
         grouped[dateKey] = [];
       }
@@ -15,38 +15,27 @@ function GroceryList({ itemList, dispatch, viewMode }) {
     }, {});
   };
 
-  const groupedItems = groupByDate(itemList);
+  const groupedItems = viewMode === 'byDate' ? groupByDate(itemList) : { 'All Items': itemList };
 
   return (
     <>
-      {viewMode === 'byDate' ? (
+      {
         Object.keys(groupedItems).map(date => (
-          <section key={date} className="grocery-section">
-            <h2 className="section-heading">{date}</h2>
+          <section key={date} className="grocery-section"> 
+            <h2 className="section-heading">{date}</h2> 
             <ul>
               {groupedItems[date].map(item_map => (
                 <GroceryItem
                   key={item_map.id}
                   itemMap={item_map}
                   dispatch={dispatch}
+                  viewMode={viewMode} 
                 />
               ))}
             </ul>
           </section>
         ))
-      ) : (
-        <section className="grocery-section">
-          <ul>
-            {itemList.map(item_map => (
-              <GroceryItem
-                key={item_map.id}
-                itemMap={item_map}
-                dispatch={dispatch}
-              />
-            ))}
-          </ul>
-        </section>
-      )}
+      }
     </>
   );
 }
