23d264214c2baf740d402e0b5efedde67385fb26
Jesus Vazquez <vapa.jesusangel@gmail.com>
2024-10-24 23:49:06 -0400

    Updated port for localhost

--- src/utils/httpReq.js
+++ src/utils/httpReq.js
@@ -1,7 +1,7 @@
 function httpReq(method_name, path_str, body_map){
 	let req_map = {
 		method : method_name,
-		url : 'http://127.0.0.1:3000' + (path_str ? path_str : '')
+		url : 'http://127.0.0.1:5173' + (path_str ? path_str : '')
 	};
 
 	if(body_map){
