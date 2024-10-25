a0bb25a6ac328e1195e8fa7597abf546fd813f29
Jesus Vazquez <vapa.jesusangel@gmail.com>
2024-10-24 23:06:16 -0400

    Added logic to handle view change

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -4,20 +4,27 @@ import httpReq from '../utils/httpReq';
 import DatePickerComponent from '../utils/DatePickerWidget.jsx';
 
 // eslint-disable-next-line react/prop-types
-function SearchBar({ dispatch }) {
+function SearchBar({ dispatch, setViewMode }) {
   const [inputValue, setInputValue] = useState('');
   const [selectedDate, setSelectedDate] = useState(new Date());
   const [isCalendarOpen, setIsCalendarOpen] = useState(false);
+  const [viewOption, setViewOption] = useState('byDate');
 
   const handleKeyDown = (event) => {
     if (event.key === 'Enter') {
       httpReq('post', '/item/create', { name: inputValue, date: selectedDate })
         .then(item_map => {
           dispatch({ type: 'add', body: item_map });
-          resetFields(); // Reset fields after successful addition
+          resetFields(); 
         })
         .catch(console.error);
     }
+  };
+
+  const handleViewChange = (event) => {
+    const newView = event.target.value;
+    setViewOption(newView); 
+    setViewMode(newView); 
   };
 
   const resetFields = () => {
@@ -26,30 +33,37 @@ function SearchBar({ dispatch }) {
   };
 
   return (
-    <div className="search-bar-container">
+    <><div className="search-bar-container">
       <input
         className="search-bar"
         type="text"
         placeholder="What would you like to buy?"
         value={inputValue}
         onChange={(e) => setInputValue(e.target.value)}
-        onKeyDown={handleKeyDown}
-      />
+        onKeyDown={handleKeyDown} />
       <div className="date-picker-wrapper">
         <span className="date-label">And when?...</span>
         <DatePickerComponent
           selectedDate={selectedDate}
           onChange={setSelectedDate}
           isOpen={isCalendarOpen}
-          toggleCalendar={() => setIsCalendarOpen(!isCalendarOpen)}
-        />
+          toggleCalendar={() => setIsCalendarOpen(!isCalendarOpen)} />
         {selectedDate && (
           <span className="date-selected">
             {selectedDate.toLocaleDateString('es-ES', { day: '2-digit', month: '2-digit', year: 'numeric' })}
           </span>
         )}
       </div>
+
     </div>
+    <div className="view-selector">
+      <label htmlFor="view">View:</label>
+      <select id="view" value={viewOption} onChange={handleViewChange}>
+        <option value="all">All Items</option>
+        <option value="byDate">By Date</option>
+      </select>
+    </div></>
+    
   );
 }
 
