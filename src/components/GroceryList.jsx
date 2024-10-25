73b01d73aa6fa6e7b73897e85f06137646781ba7
Jesus Vazquez <vapa.jesusangel@gmail.com>
2024-10-24 23:03:59 -0400

    Added ViewMode feature

--- src/components/GroceryList.jsx
+++ src/components/GroceryList.jsx
@@ -2,22 +2,53 @@
 import GroceryItem from './GroceryItem';
 import '../css/GroceryList.css';
 
-function GroceryList({ itemList, dispatch }) {
+// eslint-disable-next-line react/prop-types
+function GroceryList({ itemList, dispatch, viewMode }) { 
+
+  const groupByDate = (items) => {
+    return items.reduce((grouped, item) => {
+      const dateKey = new Date(item.date).toLocaleDateString(); 
+      if (!grouped[dateKey]) {
+        grouped[dateKey] = [];
+      }
+      grouped[dateKey].push(item);
+      return grouped;
+    }, {});
+  };
+
+  const groupedItems = groupByDate(itemList);
+
   return (
     <>
-      <ul>
-        {
-          itemList.map(function(item_map) {
-            return (
+      {viewMode === 'byDate' ? (
+        // Modo "By Date"
+        Object.keys(groupedItems).map(date => (
+          <section key={date} className="grocery-section">
+            <h2 className="section-heading">{date}</h2>
+            <ul>
+              {groupedItems[date].map(item_map => (
+                <GroceryItem
+                  key={item_map.id}
+                  itemMap={item_map}
+                  dispatch={dispatch}
+                />
+              ))}
+            </ul>
+          </section>
+        ))
+      ) : (
+        <section className="grocery-section">
+          <ul>
+            {itemList.map(item_map => (
               <GroceryItem
                 key={item_map.id}
                 itemMap={item_map}
                 dispatch={dispatch}
               />
-            );
-          })
-        }
-      </ul>
+            ))}
+          </ul>
+        </section>
+      )}
     </>
   );
 }
